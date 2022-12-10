1b6a91ed44e5b4be0505bf637996fa1f17f354c7
warjiang <1096409085@qq.com>
2022-12-10 14:48:29 +0800

    ci: add clash rules

--- convert.js
+++ convert.js
@@ -21,5 +21,5 @@ const axios = require("axios");
     console.log(finalUrl);
 
     const resp = await axios.get(finalUrl);
-    console.log(resp);
+    fs.writeFileSync('./clashr.yaml', resp.data);
 })();
